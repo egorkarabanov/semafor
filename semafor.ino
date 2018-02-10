011f69f7fd6e950e4fb9e5b61cd19c61539b0254
egorkarabanov <egor.karabanov@gmail.com>
2018-02-10 12:50:31 +0200

    Initial draft

---
+++ semafor.ino
@@ -0,0 +1,26 @@
+#define LED_R_PIN 8
+#define LED_G_PIN 7
+#define SW_PIN 6
+
+void setup(){
+  pinMode(LED_R_PIN,OUTPUT);
+  pinMode(LED_G_PIN,OUTPUT);
+  pinMode(SW_PIN,INPUT);
+}
+void loop(){
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+  
+  
